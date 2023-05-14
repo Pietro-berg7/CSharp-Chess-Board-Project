c1e42eca104dd84c4cc45e89762359f40080f4c0
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-14 03:35:15 -0300

    Criando a primeira classe do projeto

---
+++ CSharp-Chess-Board-Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using CSharp_Chess_Board_Project.Board;
+using System;
+
+namespace CSharp_Chess_Board_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position P;
+
+            P = new Position(3, 4);
+
+            Console.WriteLine("Position: " + P);
+            Console.ReadLine();
+        }
+    }
+}
