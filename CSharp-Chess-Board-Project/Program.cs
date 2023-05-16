d57dca4a36f480d719e55dda182d1c71dfc1d3a8
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-16 16:30:35 -0300

    Testes iniciais dos movimentos das peças

--- CSharp-Chess-Board-Project/Program.cs
+++ CSharp-Chess-Board-Project/Program.cs
@@ -17,11 +17,18 @@ namespace xadrez_console
                 while (!partida.terminada)
                 {
                     Console.Clear();
-
                     Tela.imprimirTabuleiro(partida.tab);
 
+                    Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossíveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossíveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
